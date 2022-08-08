a9ab5e66aac0a6782f69b863afee0a0309c3f46b
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-08-09 01:35:21 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
 module.exports = function check(str, bracketsConfig) {
-  const objConf = Object.fromEntries(bracketsConfig);
-  const openBrackets=Object.keys(objConf);
-  let stack = [];
-  for (let i=0;i< str.length;i++) {
-      if (openBrackets.includes(str[i])) {
-        if (str[i]===stack[stack.length-1] && objConf[str[i]]===str[i]) {
-          stack.pop();
-        } else {
-          stack.push(str[i]);
-        }
-      }
-      else if (stack.length ===0) {
-          return false;
-      } 
-      else if (objConf[stack[stack.length-1]]===str[i]) {
-          stack.pop();
-      } 
-      else {return false;}
-  }
-if (stack.length ===0) {return true;} 
-      else {return false;}
      };
