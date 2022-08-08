6b8ca7a5c326eafcbb8ac16f9bfa61a744e5999a
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-08-09 01:32:59 +0500

    feat:function body added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const objConf = Object.fromEntries(bracketsConfig);
+  const openBrackets=Object.keys(objConf);
+  let stack = [];
+  for (let i=0;i< str.length;i++) {
+      if (openBrackets.includes(str[i])) {
+        if (str[i]===stack[stack.length-1] && objConf[str[i]]===str[i]) {
+          stack.pop();
+        } else {
+          stack.push(str[i]);
+        }
+      }
+      else if (stack.length ===0) {
+          return false;
+      } 
+      else if (objConf[stack[stack.length-1]]===str[i]) {
+          stack.pop();
+      } 
+      else {return false;}
+  }
+if (stack.length ===0) {return true;} 
+      else {return false;}
+     };
